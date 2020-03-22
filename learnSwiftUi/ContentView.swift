32928c69d72cf0b4dde79949dec68be73fb0cb8b
Huynh Ngoc Dinh <huynhngocdinh98@gmail.com>
2020-03-22 15:44:42 +0700

    some common component

--- learnSwiftUi/ContentView.swift
+++ learnSwiftUi/ContentView.swift
@@ -8,9 +8,69 @@
 
 import SwiftUI
 
+struct MyImage: View
+{
+    static let IMAGE_DEFAULT_SIZE = CGFloat(50)
+    let imageName: String
+    let imageWidth: CGFloat
+    let imageHeight: CGFloat
+    
+    init(_ imageName: String, imageWidth: CGFloat? = IMAGE_DEFAULT_SIZE, imageHeight: CGFloat? = IMAGE_DEFAULT_SIZE) {
+        self.imageName = imageName
+        self.imageWidth = imageWidth!
+        self.imageHeight = imageHeight!
+    }
+
+    var body: some View {
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .frame(width: imageWidth, height: imageWidth)
+    }
+}
+
+struct Challenge1: View {
+    var body: some View {
+       VStack {
+            Spacer()
+            HStack {
+                Spacer()
+                MyImage("tomato")
+                Spacer()
+                MyImage("tomato")
+                Spacer()
+                MyImage("tomato")
+                Spacer()
+            }
+            Spacer()
+            HStack {
+                Spacer()
+                MyImage("tomato")
+                Spacer()
+                MyImage("tomato")
+                Spacer()
+                MyImage("tomato")
+                Spacer()
+            }
+            Spacer()
+            HStack {
+                Spacer()
+                MyImage("tomato")
+                Spacer()
+                MyImage("tomato")
+                Spacer()
+                MyImage("tomato")
+                Spacer()
+            }
+            Spacer()
+        }
+    }
+}
+
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Challenge1()
     }
 }
 
