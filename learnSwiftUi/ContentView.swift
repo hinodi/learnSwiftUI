5d1fbe8fbdaddbf602a60ce6785dbb3d679f8935
Huynh Ngoc Dinh <huynhngocdinh98@gmail.com>
2020-03-22 14:18:46 +0700

    first commit

---
+++ learnSwiftUi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  learnSwiftUi
+//
+//  Created by Huynh Ngoc Dinh on 3/22/20.
+//  Copyright © 2020 Huynh Ngoc Dinh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
