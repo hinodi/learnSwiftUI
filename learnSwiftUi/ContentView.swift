197fd3f75bc73daa2b552f27086f6e04c97aad27
Huynh Ngoc Dinh <huynhngocdinh98@gmail.com>
2020-03-22 23:13:42 +0700

    lots app

--- learnSwiftUi/ContentView.swift
+++ learnSwiftUi/ContentView.swift
@@ -141,10 +141,104 @@ struct Challenge3: View {
     }
 }
 
+struct Challenge4: View {
+    let LIST_IMAGES = ["star", "apple", "cherry"]
+    let BET_AMOUNTS = 5
+    
+    @State var credits = 1000
+    
+    @State var imageIndex1 = 0
+    @State var imageIndex2 = 1
+    @State var imageIndex3 = 2
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(Color(red: 200/255, green: 143/255, blue: 32/255))
+                .edgesIgnoringSafeArea(.all)
+            Rectangle()
+                .foregroundColor(Color(red: 228/255, green: 196/255, blue: 76/255))
+                .rotationEffect(Angle(degrees: 45))
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                HStack {
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                    Text("HINODI Slots!")
+                        .bold()
+                        .foregroundColor(.white)
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                }
+                .padding(.top, 30)
+                .padding(.horizontal)
+                .scaleEffect(2)
+                Spacer()
+                Text("Credits: " + String(credits))
+                    .foregroundColor(.black)
+                    .padding(.all, 10)
+                    .background(Color.white.opacity(0.5))
+                    .cornerRadius(20)
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(self.LIST_IMAGES[imageIndex1])
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fit)
+                        .background(Color.white.opacity(0.5))
+                        .cornerRadius(20)
+                    Image(self.LIST_IMAGES[imageIndex2])
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fit)
+                        .background(Color.white.opacity(0.5))
+                        .cornerRadius(20)
+                    Image(self.LIST_IMAGES[imageIndex3])
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fit)
+                        .background(Color.white.opacity(0.5))
+                        .cornerRadius(20)
+                    Spacer()
+                }
+                Spacer()
+                Button(action: {
+                    self.credits -= self.BET_AMOUNTS * 4
+                    
+                    let imageIndex1 = Int.random(in: 0 ... self.LIST_IMAGES.count - 1)
+                    let imageIndex2 = Int.random(in: 0 ... self.LIST_IMAGES.count - 1)
+                    let imageIndex3 = Int.random(in: 0 ... self.LIST_IMAGES.count - 1)
+                    
+                    if (imageIndex1 == imageIndex2 && imageIndex2 == imageIndex3) {
+                        self.credits += self.BET_AMOUNTS * 8
+                    } else if (imageIndex1 == imageIndex2) {
+                        self.credits += self.BET_AMOUNTS
+                    } else if (imageIndex2 == imageIndex3) {
+                        self.credits += self.BET_AMOUNTS
+                    } else if (imageIndex3 == imageIndex1) {
+                        self.credits += self.BET_AMOUNTS
+                    }
+                    
+                    self.imageIndex1 = imageIndex1
+                    self.imageIndex2 = imageIndex2
+                    self.imageIndex3 = imageIndex3
+                }) {
+                    Text("Spin")
+                        .bold()
+                        .foregroundColor(.white)
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 30)
+                        .background(Color.pink)
+                        .cornerRadius(20)
+                }
+                Spacer()
+            }
+        }
+    }
+}
 
 struct ContentView: View {
     var body: some View {
-        Challenge3()
+        Challenge4()
     }
 }
 
